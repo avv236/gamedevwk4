3563567caa29d292539f62648bd179114de1d5a0
Audrey V <avv236@nyu.edu>
2016-02-18 14:31:00 -0500

    first upload

---
+++ Assets/Scripts/CharControlDemo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharControlDemo : MonoBehaviour {
+
+// Update is called once per frame
+void Update () {
+	if(Input.GetKey(KeyCode.UpArrow)){
+		transform.Translate(0f,0f,5f * Time.deltaTime);
+		GetComponent<CharacterController>().Move(transform.forward * 5f * Time.deltaTime);
+	}
+
+	//turning left
+	if(Input.GetKey(KeyCode.LeftArrow)){
+		transform.Rotate(0f,-180f * Time.deltaTime,0f);
+	}
+	//turning right
+	if(Input.GetKey(KeyCode.RightArrow)){
+		//transform.Rotate(0f,180f * Time.deltaTime,0f);
+		transform.localEulerAngles += new Vector3(0f,180f * Time.deltaTime,0f);
+	}
+}
+}
---
+++ Assets/Scripts/DistanceDemo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceDemo : MonoBehaviour {
+
+	public Transform cube, sphere;
+
+
+	// Update is called once per frame
+	void Update () {
+		//more code, but gives you direction/orientation
+		Vector3 vectorBetweenTwoPoints = (cube.position - sphere.position);
+		if (vectorBetweenTwoPoints.magnitude > 5f && vectorBetweenTwoPoints.magnitude < 10f){
+			Debug.Log("u win");
+			cube.gameObject.SetActive(false); //disables cube
+		}else{
+			Debug.Log("u lose");
+		}
+
+	// ONLY tells you distance, no direction
+	float distance = Vector3.Distance(cube.position, sphere.position);
+	Debug.Log ("u r" + distance.ToString() + "units apart");
+	}
+}
