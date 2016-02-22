335ba3adf0754280d8b4d2a0bd42c8607783fc77
Audrey V <avv236@nyu.edu>
2016-02-21 21:44:55 -0500

    done???

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			transform.Rotate(-90f,0f,0f);
+		}
+	}
+}
